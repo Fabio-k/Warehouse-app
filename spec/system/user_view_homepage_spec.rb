e8016a23753e44ddf7bc27e06e81ce2c8571f86d
fabio-k <fabiokmain@gmail.com>
2024-10-09 07:23:10 -0300

    improve html and tests

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -19,14 +19,14 @@ describe 'user visits homepage' do
     expect(page).not_to have_content('Não existem galpões cadastrados')
 
     expect(page).to have_content('Rio')
-    expect(page).to have_content('código: SDU')
-    expect(page).to have_content('cidade: Rio de Janeiro')
-    expect(page).to have_content('60000 m2')
+    expect(page).to have_content('Código: SDU')
+    expect(page).to have_content('Cidade: Rio de Janeiro')
+    expect(page).to have_content('Área: 60000 m2')
 
     expect(page).to have_content('Maceio')
-    expect(page).to have_content('código: MCZ')
-    expect(page).to have_content('cidade: Maceio')
-    expect(page).to have_content('50000 m2')
+    expect(page).to have_content('Código: MCZ')
+    expect(page).to have_content('Cidade: Maceio')
+    expect(page).to have_content('Área: 50000 m2')
   end
 
   it 'and there are no warehouses' do
