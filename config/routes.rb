d9400b1c9139c271d498948ec77ee805597c66cd
fabio-k <fabiokmain@gmail.com>
2024-10-12 13:11:49 -0300

    feat: implement devise and customize sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'home#index'
   resources :warehouses
   resources :suppliers, only: [:index, :show, :new, :create, :edit, :update]
-resources :product_models, only: [:index, :new, :create, :show]
+  resources :product_models, only: [:index, :new, :create, :show]
 end
