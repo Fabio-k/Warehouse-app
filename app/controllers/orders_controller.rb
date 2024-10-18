3ed622a19b19864b975f06299a99328e6dee98cd
fabio-k <fabiokmain@gmail.com>
2024-10-18 16:07:20 -0300

    search for order can find similar codes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -26,6 +26,6 @@ class OrdersController < ApplicationController
 
   def search
     @code = params[:query]
-    @order = Order.find_by code: @code
+    @orders = Order.where "code LIKE ?", "%#{@code}%"
   end
 end
--- spec/system/order/user_search_order_spec.rb
+++ spec/system/order/user_search_order_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+
 describe 'user search for order' do
   it 'has to be authenticated' do
     user = User.create!(name: 'Guilherme', email: 'guilherme@gmail.com', password: 'senha123')
@@ -44,7 +46,39 @@ describe 'user search for order' do
     expect(page).to have_content "Código: #{order.code}"
     expect(page).to have_content 'Galpão Destino: SDU - Rio'
     expect(page).to have_content 'Fornecedor: Asics co'
+  end
 
+  it 'and find multiple orders' do
+    user = User.create!(name: 'Guilherme', email: 'guilherme@gmail.com', password: 'senha123')
+    first_warehouse = Warehouse.create!(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000, 
+        address: 'Avenida do Museu do Amanhã, 1000', cep: '20100-000', description: 'Galpão da zona portuária do Rio')
+    second_warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'São Paulo', area: 35_000, 
+        address: 'Avenida paulista, 1320', cep: '20100-000', description: 'Galpão para armazenamento de entregas internacionais')
+    
+    supplier = Supplier.create!(brand_name: 'Asics', corporate_name: 'Asics co', registration_number: '2345-032', 
+    full_address: 'Av João Pessoa, 234', city: 'Manaus', state: 'Amazonas', email: 'asics@asics.com')
 
+    allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('SDU12345')
+    first_order = Order.create!(warehouse: first_warehouse, supplier: supplier, user: user, estimated_delivery_date: 1.day.from_now)
+    allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('SDU54321')
+    second_order = Order.create!(warehouse: first_warehouse, supplier: supplier, user: user, estimated_delivery_date: 1.day.from_now)
+    allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('GRU12345')
+    third_order = Order.create!(warehouse: second_warehouse, supplier: supplier, user: user, estimated_delivery_date: 1.day.from_now)
+  
+    login_as(user)
+    visit root_path
+
+    within 'nav' do
+      fill_in 'Buscar Pedido', with: 'SDU'
+      click_on 'Buscar'  
+    end
+
+    expect(page).to have_content "Resultados da busca por: SDU"
+    expect(page).to have_content '2 pedidos encontrado'
+    expect(page).to have_content "SDU12345"
+    expect(page).to have_content 'SDU54321'
+    expect(page).to have_content 'Galpão Destino: SDU - Rio'
+    expect(page).not_to have_content 'GRU12345'
+    expect(page).not_to have_content 'Galpão Destino: GRU - Aeroporto SP'
   end
 end
